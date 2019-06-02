d8731eaf965b828e5bdf924442fc45d56e7e259f
Thiago Farias <thiago_fdutra@hotmail.com>
2019-06-02 19:18:43 -0300

    changes

--- app/Controllers/Http/ChatController.js
+++ app/Controllers/Http/ChatController.js
@@ -1,10 +1,93 @@
 'use strict'
-const Chat = use('App/Models/Chat')
 
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+  @typedef {import('@adonisjs/framework/src/View')} View */
+    
+    
+  esourceful controller for interacting with chats
+  
 class ChatController {
-    async getChatList() {
+  *
+   Show a list of all chats.
+   * GET chats
+  
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
 
-    }
+  /**
+   * Render a form to be used for creating a new chat.
+   * GET chats/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new chat.
+   * POST chats
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single chat.
+   * GET chats/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing chat.
+   * GET chats/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update chat details.
+   * PUT or PATCH chats/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a chat with id.
+   * DELETE chats/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
 }
 
 module.exports = ChatController
